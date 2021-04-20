b95665c3b5dd83d9c9900307b57be40b8dec99aa
dandan1232 <345652127@qq.com>
2021-04-20 11:21:33 +0800

    项目中的常量、枚举、异常210420

---
+++ src/main/java/online/danbao/mall/api/common/MallException.java
@@ -0,0 +1,23 @@
+package online.danbao.mall.api.common;
+
+/**
+ * @author: 蛋宝
+ * @date: 2021/4/20 10:48
+ * @description: 自定义异常
+ */
+public class MallException extends RuntimeException{
+    public MallException(){
+    }
+    public MallException(String message){
+        super(message);
+    }
+
+    /**
+     * 抛出异常
+     *
+     * @param message 消息
+     */
+    public static void fail(String message){
+        throw new MallException(message);
+    }
+}
