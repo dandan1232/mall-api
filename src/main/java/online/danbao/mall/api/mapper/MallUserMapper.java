0974f479095694bdf8cc6b8a856103241590d547
dandan1232 <345652127@qq.com>
2021-04-22 14:45:03 +0800

    MallUserService实现及测试

--- src/main/java/online/danbao/mall/api/mapper/MallUserMapper.java
+++ src/main/java/online/danbao/mall/api/mapper/MallUserMapper.java
@@ -10,29 +10,72 @@ import java.util.List;
  */
 public interface MallUserMapper {
 
+    /**
+     * 根据主键删除记录
+     *
+     * @param id 主键
+     * @return int
+     */
     int deleteByPrimaryKey(Long id);
 
-    int insert(MallUser record);
+    /**
+     * 新增用户
+     *
+     * @param mallUser 入参
+     * @return 受影响的记录行数
+     */
 
-    int insertSelective(MallUser record);
-
-    MallUser selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(MallUser record);
-
-    int updateByPrimaryKey(MallUser record);
-
-    List<MallUser> selectAll();
+    int insert(MallUser mallUser);
 
     /**
-     * 根据登录名查询用户 * @param loginName 登录名 * @return 用户对象
+     * 选择性保存数据——新增用户
+     *
+     * @param mallUser 入参
+     * @return 受影响的记录行数
+     */
+    int insertSelective(MallUser mallUser);
+
+    /**
+     * 根据主键查询记录
+     *
+     * @param id 主键
+     * @return MallUser
+     */
+    MallUser selectByPrimaryKey(Long id);
+
+    /**
+     * 选择性更新
+     *
+     * @param mallUser 入参
+     * @return int
+     */
+    int updateByPrimaryKeySelective(MallUser mallUser);
+
+    /**
+     * 更新用户信息
+     *
+     * @param mallUser 入参
+     * @return int
+     */
+    int updateByPrimaryKey(MallUser mallUser);
+
+
+
+    /**
+     * 根据登录名查询用户
+     * * @param loginName 登录名
+     * * @return 用户对象
      */
     MallUser selectByLoginName(String loginName);
 
     /**
-     * 根据登录名和密码查询用户 * @param loginName 登录名 * @param password 密码 * @return 用户对象
+     * 根据登录名和密码查询用户
+     * * @param loginName 登录名
+     * * @param password 密码
+     * * @return 用户对象
      */
     MallUser selectByLoginNameAndPasswd(@Param("loginName") String loginName, @Param("password") String password);
+
 }
 
 
--- src/test/java/online/danbao/mall/api/service/MallUserServiceTest.java
+++ src/test/java/online/danbao/mall/api/service/MallUserServiceTest.java
@@ -1,26 +1,22 @@
-package online.danbao.mall.api.mapper;
+package online.danbao.mall.api.service;
 
 import lombok.extern.slf4j.Slf4j;
 import online.danbao.mall.api.controller.param.MallUserUpdateParam;
-import online.danbao.mall.api.entity.MallUser;
-import online.danbao.mall.api.service.MallUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author: 蛋宝
- * @date: 2021/4/21 9:29
+ * @date: 2021/4/22 14:38
  * @description:
  */
 @SpringBootTest
 @Slf4j
-class MallUserMapperTest {
+class MallUserServiceTest {
     @Resource
     private MallUserService mallUserService;
 
@@ -33,14 +29,12 @@ class MallUserMapperTest {
 
     @Test
     void login() {
-        String login = mallUserService.login("13300001111", "4297f44b13955235245b2497399d7a93");
-        //返回了登录用户的token，会在tb_mall_user_token表生成一条token记录
+        String login = mallUserService.login("13300001111", "4297f44b13955235245b2497399d7a93");        //返回了登录用户的token，会在tb_mall_user_token表生成一条token记录
         log.info(login);
     }
 
     @Test
-    void updateUserInfo() {
-        //修改10号用户信息,可修改的字段为MallUserUpdateParam的属性
+    void updateUserInfo() {        //修改10号用户信息,可修改的字段为MallUserUpdateParam的属性
         MallUserUpdateParam mup = new MallUserUpdateParam();
         mup.setNickName("新昵称");
         mup.setPasswordMd5("e10adc3949ba59abbe56e057f20f883e");
@@ -51,8 +45,7 @@ class MallUserMapperTest {
     }
 
     @Test
-    void logout() {
-        //会在tb_mall_user_token表中删掉10号用户的一条token数据
+    void logout() {        //会在tb_mall_user_token表中删掉10号用户的一条token数据
         Boolean logout = mallUserService.logout(10L);
         log.info(logout.toString());
         assertTrue(logout);
